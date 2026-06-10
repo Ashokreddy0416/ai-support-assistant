b4a735f4ad216016fab690c7cc0cde4ecc1ab9fe
ashokreddy <asreddy@technosofteng.com>
2026-06-10 17:12:00 +0530

    Phase 10: scope pytest to tests dir, lazy-init neo4j driver

--- backend/graph.py
+++ backend/graph.py
@@ -3,12 +3,20 @@ from dotenv import load_dotenv
 from neo4j import GraphDatabase
 
 load_dotenv()
-driver = GraphDatabase.driver(
-    os.environ["NEO4J_URI"],
-    auth=(os.environ["NEO4J_USERNAME"], os.environ["NEO4J_PASSWORD"]),
-)
+
+_driver = None
+
+def _get_driver():
+    global _driver
+    if _driver is None:
+        _driver = GraphDatabase.driver(
+            os.environ["NEO4J_URI"],
+            auth=(os.environ["NEO4J_USERNAME"], os.environ["NEO4J_PASSWORD"]),
+        )
+    return _driver
 
 def related_pages(start_url: str, hops: int = 2) -> list[str]:
+    driver = _get_driver()
     query = (
         "MATCH (a:Page {url: $start})-[:LINKS_TO*1..%d]->(b:Page) "
         "RETURN DISTINCT b.url AS url LIMIT 10" % hops
